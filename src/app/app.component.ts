6963755afdc5c4d47c989e726294b8f54a550222
IsaiasPC <isamed92@gmail.com>
2018-12-19 00:55:17 -0300

    new pipes added, until async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve, reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Isaias';
-
+  PI = Math.PI;
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  a = 0.234;
+  salario = 1234.5;
+  
+
+  heroe = {
+    clave: 'wolverine',
+    nombre: 'logan',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: '19'
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject) =>{
+    setTimeout( () => resolve('llego la data'),  3500);
+  } );
 }
